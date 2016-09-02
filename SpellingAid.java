6c9873716caa47044c49754945768f460bafd541
st970703 <st970703@gmail.com>
2016-09-02 13:42:29 +1200

    Add files via upload
    Testing uploading Wayne's Assignment 2 Java files

---
+++ SpellingAid.java
@@ -0,0 +1,158 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+/**
+ * This class creates the GUI and required objects to run the program.
+ * @author wayne
+ *
+ */
+public class SpellingAid extends JFrame implements ActionListener{
+
+	// many many Swing components
+	private JTextField inputText = new JTextField();
+	private JButton newQuizBtn = new JButton("New Spelling Quiz");
+	private JButton reviewMistakesBtn = new JButton("Review Mistakes");
+	private JButton viewStatsBtn = new JButton("View Statistics");
+	private JButton clearStatsBtn = new JButton("Clear Statistics");
+	
+	private JPanel menuBtns = new JPanel(new GridLayout(0, 1));
+	private JPanel inputArea = new JPanel(new GridLayout(0, 1));
+	private JTextArea previousInput = new JTextArea("Please select one of the options to the left.");
+	private JLabel instructions = new JLabel();
+	
+	private Quiz _currentQuiz;
+	
+	/**
+	 * Initializes swing components.
+	 */
+	public SpellingAid() {
+		super("Spelling Aid V2.0");
+		setSize(600, 400);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		newQuizBtn.addActionListener(this);
+		reviewMistakesBtn.addActionListener(this);
+		viewStatsBtn.addActionListener(this);
+		clearStatsBtn.addActionListener(this);
+		inputText.addActionListener(this);
+		inputText.setEnabled(false);
+		menuBtns.add(newQuizBtn);
+		menuBtns.add(reviewMistakesBtn);
+		menuBtns.add(viewStatsBtn);
+		menuBtns.add(clearStatsBtn);
+		inputArea.add(instructions);
+		inputArea.add(inputText);
+		add(inputArea, BorderLayout.SOUTH);
+		add(menuBtns, BorderLayout.WEST);
+		add(previousInput, BorderLayout.CENTER);
+		previousInput.setEditable(false);
+		previousInput.setPreferredSize(new Dimension(300, 300));
+	}
+	
+	/**
+	 * Performs different actions, depending on what action was performed.
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String action = e.getActionCommand();
+		
+		if (action.equals(newQuizBtn.getActionCommand())) {
+			removeQuizListeners();
+			_currentQuiz = new Quiz(QuizType.NEW);
+			inputText.addActionListener(_currentQuiz);
+			instructions.setText("Spell the word below and press Enter: ");
+			inputText.setEnabled(true);
+			previousInput.setText("");
+			inputText.requestFocusInWindow();
+			boolean status = _currentQuiz.sayNextWord();
+			if (!status) { inputText.setEnabled(false); };
+		} else if (action.equals(reviewMistakesBtn.getActionCommand())) {
+			removeQuizListeners();
+			_currentQuiz = new Quiz(QuizType.REVIEW);
+			inputText.addActionListener(_currentQuiz);
+			instructions.setText("Spell the word below and press Enter: ");
+			inputText.setEnabled(true);
+			previousInput.setText("");
+			inputText.requestFocusInWindow();
+			boolean status = _currentQuiz.sayNextWord();
+			if (!status) { inputText.setEnabled(false); };
+		} else if (action.equals(viewStatsBtn.getActionCommand())) {
+			instructions.setText("");
+			previousInput.setText("");
+			inputText.setEnabled(false);
+			Statistics stats = new Statistics();
+			ArrayList<String> formattedStats = stats.getStats();
+			
+			for (String line : formattedStats) {
+				previousInput.append(line);
+			}
+		} else if (action.equals(clearStatsBtn.getActionCommand())) {
+			instructions.setText("");
+			previousInput.setText("");
+			inputText.setEnabled(false);
+			Statistics stats = new Statistics();
+			stats.clearStats();
+			clearList(".failedlist");
+			clearList(".faultedlist");
+		} else {
+			previousInput.setText(previousInput.getText() + action + "\n");
+			inputText.setText("");
+		}
+	}
+	
+	/**
+	 * Clears the given list by deleting it and re-making it.
+	 * @param list - the list the clear
+	 */
+	private void clearList(String list) {
+		File listFile = new File(list);
+		
+		if (listFile.exists()) {
+			listFile.delete();
+			try {
+				listFile.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * Removes old Quiz ActionListeners from the input field.
+	 */
+	private void removeQuizListeners() {
+		ActionListener[] listeners = inputText.getActionListeners();
+		for (ActionListener listener : listeners) {
+			if (listener != this) {
+				inputText.removeActionListener(listener);
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				SpellingAid frame = new SpellingAid();
+				frame.setVisible(true);
+			}
+			
+		});
+	}
+}
+
